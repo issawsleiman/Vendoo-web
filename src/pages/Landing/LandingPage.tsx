a02855aa3506fd593db675a65d98963c09d0c1a9
issawsleiman <issasleiman.intern@anghami.com>
2025-10-15 22:55:10 +0300

    a

---
+++ src/pages/Landing/LandingPage.tsx
@@ -0,0 +1,16 @@
+import VendooHeader from "../../components/common/VendooHeader";
+import HeroSection from "../../components/layout/HeroSection";
+
+function Landing() {
+  return (
+    <>
+      <VendooHeader />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+    </>
+  );
+}
+
+export default Landing;
